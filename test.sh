69793b5fdc1f021a1fd9fc1b288e8b93a2858ca9
schmidtp0740 <schmidtp0740@gmail.com>
2018-10-31 11:18:09 -0500

    added test functions for heart rate

--- test.sh
+++ test.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 clear
 
+# printf "GetPeople\n"
 # curl -H "Content-type:application/json" -X GET http://localhost:8080/pd 
 # printf "\n"
-# curl -H "Content-type:application/json" -X POST http://localhost:4001/bcsgw/rest/v1/transaction/query -d '{"channel":"mychannel","chaincode":"emrcc","method":"getPeople","args":[],"chaincodeVer": "v1" }'
-# printf "\n"
 
+# printf "GetPerson\n"
 # curl -H "Content-type:application/json" -X GET http://localhost:8080/pd/p01
 # printf "\n"
-# curl -H "Content-type:application/json" -X POST http://localhost:4001/bcsgw/rest/v1/transaction/query -d '{"channel":"mychannel","chaincode":"emrcc","method":"getPerson","args":["p01"],"chaincodeVer": "v1" }'
+
+# printf "insertRx\n"
+# curl -H "Content-type:application/json" -X POST http://localhost:8080/rx -d '{"patientID":"p01","rxid":"rx01","timestamp":15123123,"doctor":"doctor","prescription":"pre","refills":1,"status":"prescribed","approved":"true"}'
 # printf "\n"
 
-curl -H "Content-type:application/json" -X POST http://localhost:8080/rx -d '{"patientID":"p01","rxid":"rx01","timestamp":15123123,"doctor":"doctor","prescription":"pre","refills":1,"status":"prescribed","approved":"true"}'
+# printf "modifyRx\n"
+# curl -H "Content-type:application/json" -X PATCH http://localhost:8080/rx -d '{"patientID":"p01","rxid":"rx01","timestamp":15123124,"doctor":"doctor","pharmacist": "phar", "prescription":"pre","refills":1,"status":"filled","approved":"true"}'
+# printf "\n"
+
+# printf "getRxForPatient\n"
+# curl -H "Content-type:application/json" -X GET http://localhost:8080/rx/p01 -d '{"patientID":"p01"}'
+# printf "\n"
+
+printf "insertHeartRate\n"
+curl -H "Content-type:application/json" -X POST http://localhost:8080/hr -d ''
 printf "\n"
 
-curl -H "Content-type:application/json" -X PATCH http://localhost:8080/rx -d '{"patientID":"p01","rxid":"rx01","timestamp":15123124,"doctor":"doctor","pharmacist": "phar", "prescription":"pre","refills":1,"status":"filled","approved":"true"}'
-printf "\n"
-
-curl -H "Content-type:application/json" -X GET http://localhost:8080/rx/p01 -d '{"patientID":"p01"}'
-printf "\n"
+# printf "getHeartRateDataForPatient\n"
+# curl -H "Content-type:application/json" -X GET http://localhost:8080/hr/p01 -d ''
+# printf "\n"
